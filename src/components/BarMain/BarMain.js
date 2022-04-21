46b0e62108b3ed75ba25cacee3a558ce2039629c
rafikamirkarimova <tekhronkhudaykulov@gmail.com>
2022-04-21 06:30:46 +0500

    21.04.2022

--- src/components/BarMain/BarMain.js
+++ src/components/BarMain/BarMain.js
@@ -4,7 +4,6 @@ import { Outlet, useNavigate } from "react-router-dom";
 import Curiers from "../Curiers";
 import Orders from "../Orders";
 import AllCurierMap from "../Map";
-import { NavLink } from "react-router-dom";
 
 const BarMain = () => {
   const navigate = useNavigate();
